15b90b2665955465202c6bfb14852c7be3c2037e
Samaelo <samaelpicazo16@gmail.com>
2016-12-10 13:02:21 +0100

    ASAAAAAAAAAAAAA

--- app/src/main/java/model/AccesoFicheros.java
+++ app/src/main/java/model/AccesoFicheros.java
@@ -1,6 +1,10 @@
 package model;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 
 
@@ -32,7 +36,7 @@ public class AccesoFicheros{
             bufferedWriter.write("Joputaaaaaaaaaa");
         }
         catch (IOException e) {
-            System.out.println("Se ha producido un error de E/S");
+            System.out.println(e.getMessage());
         }
 
         finally{
@@ -71,6 +75,40 @@ public class AccesoFicheros{
                     System.out.println(" No se ha podido leer el fichero.");
                 }
             }
+        }
+    }
+
+
+    // -- PETICIÓN DE PERSMISOS -- //
+
+    /**
+     * Constante de tipo entero que es utilizada para verificar los permisos sobre la memoria externa.
+     */
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+
+    /**
+     * Array de constantes de tipo String que ayudan a conocer si los permisos garantizados son los deseados. Dentro del Array pedimos los permisos de lectura y escritura del fichero.
+     */
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    /**
+     * Verifica que los permisos de escritura en la memoria externa están garantizados.
+     * @param activity - La actividad que desea verificar los
+     */
+    public static void verificarPermisosDeAlmacenamiento(Activity activity) {
+
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
         }
     }
 }
